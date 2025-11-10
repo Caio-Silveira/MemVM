289aad0f2903ae3ce4e3a78ff0c0ebac1e248836
Silverdev <82159533+Caio-Silveira@users.noreply.github.com>
2025-11-10 12:14:13 -0300

    Initial commit: added README and project structure

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        Log("DLL injected successfully!");
+        Log("DLL injected successfully");
         break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
