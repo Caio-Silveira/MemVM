c3acd4b1d336c03f197ba37fad2310ecb09cfbe2
Silverdev <82159533+Caio-Silveira@users.noreply.github.com>
2025-11-11 21:26:26 -0300

    feat: added VM function type definitions

---
+++ include/Vmi/Vmi_Types.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <cstdint>
+
+namespace MemVM::Vmi {
+
+    using PGMPhysReadType = int(__cdecl*)(void*, uintptr_t, void*, size_t);
+    using PGMPhysWriteType = int(__cdecl*)(void*, uintptr_t, void*, size_t);
+    using PGMPhysGCPtr2GCPhysType = int(__cdecl*)(void*, uintptr_t, uintptr_t*);
+    using VMMGetCpuByIdType = void*(__cdecl*)(void*, int);
+
+    struct FunctionPointers {
+        PGMPhysReadType read;
+        PGMPhysWriteType write;
+        PGMPhysGCPtr2GCPhysType convert;
+        VMMGetCpuByIdType getCpu;
+    };
+
+    inline FunctionPointers Target;
+
+    inline FunctionPointers Original;
+
+}
