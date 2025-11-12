c5c888d4236a9e3bcfe73980573c4d1289ff8641
Silverdev <82159533+Caio-Silveira@users.noreply.github.com>
2025-11-12 12:17:51 -0300

    feat: added Vmi start/stop

--- main.cpp
+++ main.cpp
@@ -1,29 +1,16 @@
+#include "Vmi/Vmi.h"
 #include <windows.h>
 
-void Log(const char* msg)
-{
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (!hConsole)
-    {
-        AllocConsole();
-        hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    }
-    DWORD written;
-    WriteConsoleA(hConsole, msg, (DWORD)strlen(msg), &written, nullptr);
-    WriteConsoleA(hConsole, "\n", 1, &written, nullptr);
-}
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved) {
+    switch (reason) {
+        case DLL_PROCESS_ATTACH:
+            DisableThreadLibraryCalls(hModule);
+            MemVM::Vmi::Start("MemVM_BlueStacks");
+            break;
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
-{
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-        Log("DLL injected successfully");
-        break;
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
+        case DLL_PROCESS_DETACH:
+            MemVM::Vmi::Stop();
+            break;
     }
     return TRUE;
 }
